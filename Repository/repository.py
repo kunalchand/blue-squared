5eb8fc14b458753862fa03e93501ea3523f867eb
MandavaSaiSujith <117120683+MandavaSaiSujith@users.noreply.github.com>
2025-01-22 13:23:08 -0600

    Pushing repository.py only with small changes

--- Repository/repository.py
+++ Repository/repository.py
@@ -1,16 +1,10 @@
 # repository.py
 
-import sys
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from Config.config import get_db_url  # Import get_db_url to dynamically get the DB URL
 from Model.models import Person, Person2
 
-# Add the project root directory (blue-squared) to the Python path
-script_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.abspath(os.path.join(script_dir, '..', '..'))
-sys.path.append(project_root)
 
 class DatabaseRepository:
     def __init__(self):
